92d9e41a0f7e556398e483314cb5bd8a40b9e5b0
Gustavo Guichard <gustavoguichard@gmail.com>
2012-10-07 00:45:05 -0300

    Prevents not approved users to see the content

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -2,18 +2,20 @@ class UserAbility
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :all
-    can :cocreate, Idea
-    can :create, Idea
-    can :create, Message
-    can :colaborate, Idea do |idea|
-      idea.user != user
-    end
-    can :manage, Idea, :user => user
-    can :notifications, User
-    can :update_notification, User
-    can :manage, User, :id => user.id
+    if user.approved
+      can :read, :all
+      can :cocreate, Idea
+      can :create, Idea
+      can :create, Message
+      can :colaborate, Idea do |idea|
+        idea.user != user
+      end
+      can :manage, Idea, :user => user
+      can :notifications, User
+      can :update_notification, User
+      can :manage, User, :id => user.id
 
-    can :read, TimelineController
+      can :read, TimelineController
+    end
   end
 end
