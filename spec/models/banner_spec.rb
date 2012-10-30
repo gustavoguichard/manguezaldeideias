25ae3ed6a804c638dc59dfbdacefa258f884b404
Gustavo Guichard <gustavoguichard@gmail.com>
2012-10-30 16:25:58 -0200

    Fixed specs

--- spec/models/banner_spec.rb
+++ spec/models/banner_spec.rb
@@ -5,9 +5,6 @@ describe Banner do
   describe "Validations/Associations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
-    it { should validate_presence_of :link_text }
-    it { should validate_presence_of :link_url }
-    it { should validate_presence_of :image_url }
   end
 
   describe "#latest" do
